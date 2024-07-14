cf2b2840460b5de743da5dd057c0848567ca1f49
dzikoysk <dzikoysk@dzikoysk.net>
2024-07-14 23:46:15 +0200

    [S-0000] Support series + Reposilite 3.x backstage series intro (Resolve #9)
    * GH-9 Support series
    
    * part 2
    
    * part 3
    
    * part 4
    
    * part 5
    
    * part 6
    
    * cr
    
    * adjust margins in header on mobile
    
    * margins v2
    
    * margins v3
    
    * impl intro for reposilite 3x backstage as an example chapter

--- src/pages/series.jsx
+++ src/pages/series.jsx
@@ -12,7 +12,7 @@ export async function getStaticProps() {
   }
 }
 
-export default function Series({ series }) {
+export default function Series({series}) {
   return (
     <>
       <Head>
@@ -20,46 +20,61 @@ export default function Series({ series }) {
       </Head>
       <Layout>
         <div className={'flex flex-col w-full'}>
-          {series.map(({details, chapters}, index) => (
-            <div key={index} className={`w-full pt-6 px-4`}>
-              <div className={`flex flex-col lg:flex-row lg:max-h-52 w-full bg-white rounded-lg`}>
-                <img
-                  src={`/series/${details.path}/${details.image}`}
-                  alt={details.title}
-                  className={`bg-white rounded-l-lg lg:w-96 h-52 ,ax object-cover`}
-                />
-                <div className={`flex flex-1 flex-col px-4 py-2`}>
-                  <div>
-                    <h2 className={`text-2xl px-2 pt-1 rounded-md font-semibold`}>
-                      {details.title}
-                    </h2>
-                    <p className={`px-2 text-sm text-purple-700`}>
-                      by <a href={`/${details.authors}`}>{details.authors}</a>
-                    </p>
-                  </div>
-                  <div className={`h-full flex px-2 py-2 rounded-md mt-2 w-full`}>
-                  <div className={`flex justify-between`}>
-                      <p className={`text-gray-500 w-2/5 text-sm`}>
-                        {details.description}
+          {series.map(({details, chapters}, index) => {
+            const firstChapterUrl = `/series/${details.url}/1`
+
+            return (
+              <div key={index} className={`w-full pt-6 px-4`}>
+                <div className={
+                  `flex flex-col lg:flex-row lg:max-h-52 w-full bg-white rounded-lg cursor-pointer hover:scale-[1.02] hover:duration-200`
+                }>
+                  <a href={firstChapterUrl} className={`w-full lg:w-96 h-52`}>
+                    <img
+                      src={`/series/${details.image}`}
+                      alt={details.title}
+                      className={`bg-white rounded-l-lg h-full object-cover w-full`}
+                    />
+                  </a>
+                  <div className={`flex flex-1 flex-col px-4 py-2`}>
+                    <div>
+                      <a href={firstChapterUrl}>
+                        <h2 className={`text-2xl px-2 pt-1 rounded-md font-semibold`}>
+                          {details.title}
+                        </h2>
+                      </a>
+                      <p className={`px-2 text-sm`}>
+                        by&nbsp;
+                        <a href={`/${details.authors}`} className={'text-purple-700'}>
+                          {details.authors}
+                        </a>
                       </p>
-                      <div className={`flex flex-1 flex-col pl-10 pr-4 -mt-6`}>
-                        <p className={`text-gray-400 font-semibold text-xs`}>
-                          Chapters
+                    </div>
+                    <div className={`h-full flex px-2 py-2 rounded-md mt-2 w-full`}>
+                      <div className={`flex justify-between`}>
+                        <p className={`text-gray-500 w-2/5 text-sm`}>
+                          <a href={firstChapterUrl}>
+                            {details.description}
+                          </a>
                         </p>
-                        {chapters.map((chapter, idx) => (
-                          <div key={idx} className={`flex flex-col mt-2`}>
-                            <a href={`/series/${details.path}/${chapter.url}`}>
-                              <p className={`text-purple-700 text-sm`}>{idx + 1}. {chapter.title}</p>
-                            </a>
-                          </div>
-                        ))}
+                        <div className={`flex flex-1 flex-col pl-10 pr-4 -mt-6`}>
+                          <p className={`text-gray-400 font-semibold text-xs`}>
+                            Chapters
+                          </p>
+                          {chapters.map((chapter, idx) => (
+                            <div key={idx} className={`flex flex-col mt-2`}>
+                              <a href={`/series/${details.url}/${chapter.order}`}>
+                                <p className={`text-purple-700 text-sm`}>{idx + 1}. {chapter.shortTitle}</p>
+                              </a>
+                            </div>
+                          ))}
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            )
+          })}
         </div>
       </Layout>
     </>
