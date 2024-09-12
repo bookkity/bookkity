865c1bec7de77d52c476480a9ab09b85f3f1c491
dzikoysk <dzikoysk@dzikoysk.net>
2024-09-12 18:16:53 +0200

    GH-18 Suport latex (Resolve #18)

--- src/helpers/mdx.js
+++ src/helpers/mdx.js
@@ -2,16 +2,19 @@ import {serialize} from "next-mdx-remote/serialize"
 import remarkGfm from "remark-gfm"
 import rehypeSlug from "rehype-slug"
 import rehypePrettyCode from "rehype-pretty-code"
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export function serializeMdx(mdx) {
   return serialize(mdx, {
     mdxOptions: {
       remarkPlugins: [
-        remarkGfm,
+        remarkGfm, remarkMath
       ],
       rehypePlugins: [
         // rehypePrism,
         rehypeSlug,
+        rehypeKatex,
         [rehypePrettyCode, { theme: "min-light"}],
         // rehypeAutolinkHeadings
       ]
