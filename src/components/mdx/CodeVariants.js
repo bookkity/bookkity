fa7659de6c6ebac7d7233c9bf1cd5a49cefd9653
dzikoysk <dzikoysk@dzikoysk.net>
2025-06-29 19:06:53 +0200

    GH-3 Cleanup

--- src/components/mdx/CodeVariants.js
+++ src/components/mdx/CodeVariants.js
@@ -21,11 +21,11 @@ export const CodeVariants = ({ tabs, symbol, children }) => {
                         <TabsTrigger key={props.name} value={props.name}>{props.name}</TabsTrigger>
                     ))}
                 </TabsList>
-                {children.map(variant => (
-                    <TabsContent key={variant.props.name} value={variant.props.name} paddingY={2} paddingX={6}>
+                {children.map(((variant, idx) => (
+                    <TabsContent key={`code-variant-${idx}-${variant.props.name}`} value={variant.props.name} paddingY={2} paddingX={6}>
                         {variant}
                     </TabsContent>
-                ))}
+                )))}
             </Tabs>
         )
     }
@@ -34,27 +34,43 @@ export const CodeVariants = ({ tabs, symbol, children }) => {
 
     return (
         <div className={`code-variants w-full flex flex-col lg:flex-row pt-5 pb-3 relative max-w-full justify-center`}>
-            {children.map((variant, idx) => (
-                <>
-                    {symbol && idx % 2 === 1 && (
-                        <div className={'flex justify-center items-center py-3 lg:px-3 lg:py-0'}>
-                            <span className={`text-xl rotate-90 lg:rotate-0`}>
-                                →
-                            </span>
+            {
+              children
+                .flatMap((variant, idx) => {
+                    const elements = []
+                    if (symbol && idx % 2 === 1) {
+                      elements.push(
+                        <div
+                          key={`code-variant-symbol-${idx}-${variant.props.name}`}
+                          className={'flex justify-center items-center py-3 lg:px-3 lg:py-0'}
+                        >
+                          <span className={`text-xl rotate-90 lg:rotate-0`}>
+                              →
+                          </span>
                         </div>
-                    )}
-                    <div key={idx} className={`code-variant flex flex-col lg:px-1 first:pl-0 last:pr-0 max-w-full flex-1`}>
+                      )
+                    }
+
+                    elements.push(
+                      <div
+                        key={`code-variant-${idx}-${variant.props.name}`}
+                        className={`code-variant flex flex-col lg:px-1 first:pl-0 last:pr-0 max-w-full flex-1`}
+                      >
                         <div className={''}>
-                            <div className={'text-center whitespace-pre py-2 px-20 font-semibold bg-gray-50 rounded-md'}>
-                                {variant.props.name}
-                            </div>
+                          <div
+                            className={'text-center whitespace-pre py-2 px-20 font-semibold bg-gray-50 rounded-md'}>
+                            {variant.props.name}
+                          </div>
                         </div>
                         <div className={`text-sm`}>
-                            {variant}
+                          {variant}
                         </div>
-                    </div>
-                </>
-            ))}
+                      </div>
+                    )
+
+                  return elements
+                })
+            }
         </div>
     )
 }
